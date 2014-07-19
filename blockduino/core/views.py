f2a9c7d0e7d235ac50cb74a20a4af695db5e7daf
Ankush Gola <ankush@Ankushs-MacBook-Air.local>
2014-07-19 14:01:19 -0700

    completed outline of new mod view

--- blockduino/core/views.py
+++ blockduino/core/views.py
@@ -1,37 +1,66 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
-from core.models import HistoryNode
 from datetime import datetime
 import json
 import cPickle as pickle
 from django.views.generic import TemplateView
+import uuid
+import jsonpickle
+
+modules = set(['accel', 'servo', 'led', 'push_button'])
 
 class Accel:
-	def __init__(self):
-		pass
+	def __init__(self, data):
+		self.data_pin = data['data_pin']
+		self.power = data['power_pin']
+		self.ground = data['ground_pin']
+		self.attribs = ['turn_up', 'turn_down', 'turn_left', 'turn_right']
 
 class Servo:
-	pass
+	def __init__(self, data):
+		self.data_pin = data['data_pin']
+		self.power = data['power_pin']
+		self.ground = data['ground_pin']
+		self.attribs = ['rotate_left', 'rotate_right', 'center']
 
 class LED:
-	pass
-
-class LEDGroup:
-	pass
+	def __init__(self, data):
+		self.power = data['power_pin']
+		self.ground = data['ground_pin']
+		self.attribs = ['turn_on', 'turn_off']
 
 class PushButton:
-	pass
+	def __init__(self, data):
+		self.power = data['power_pin']
+		self.ground = data['ground_pin']
+		self.attribs = ['turn_on', 'turn_off']
 
-def new_module(request, mod_name):
-	modules = set(['accel', 'servo', 'led', 'led_group', 'push_button'])
+def new_module(request):
+	payload = json.loads(request.body)
+	mod_name = payload['name']
+
 	if mod_name not in modules:
 		resp = HttpResponse()
 		resp.status_code = 401
 		return resp
 	else:
 		if mod_name == 'accel':
-			pass
+			accel = Accel(payload['pin_info'])
+			#save in fs
+		elif mod_name == 'servo':
+			servo = Servo(payload['pin_info'])
+			#save in fs
+		elif mod_name == 'led':
+			led = LED(payload['pin_info'])
+			#save in fs
+		else:
+			push_button = PushButton(payload['pin_info'])
+			#save in fs
+
+
+
+
 
 class HomeView(TemplateView):
 	template_name = 'home.html'
