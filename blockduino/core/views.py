c000d1a29dacd597e3e01fb8a6a4dcd1bb63fbea
Ankush Gola <ankush@Ankushs-MacBook-Air.local>
2014-07-20 07:57:34 -0700

    added compiler logic

--- blockduino/core/views.py
+++ blockduino/core/views.py
@@ -45,23 +45,13 @@ class PushButton:
 		self.name = 'push_button'
 		self.sense = True
 
-s_dict = {
-	'W1':['pb1', 'is_on'], 'D1': {'M': ['led1', 'turn_on']}, 'W2': ['!', 'pb1', 'is_on'], 'D2' : {'M' : ['led1', 'turn_off']}
-	}
-
-l1 = [['B_PushButton', 'pb1'], ['B_LED', 'led1']]
-
-l2 = [['pb1', 'init', '9'], ['led1', 'init', '4']]
-
 def compiler(request):
-	"""
 	payload = json.loads(request.body)
-	j_dict = payload['j_dict']
-	l1 = payload['l_dict']
-	s1 = payload['s_dict']
-	"""
+	w_dict = payload['whendo_dict']
+	s_list = payload['setup_list']
+	i_list = payload['init_list']
 
-	c1 = Compiler(s_dict, l2, l1)
+	c1 = Compiler(w_dict, s_list, i_list)
 	save_path = '/Users/ankush/blockduino/src/'
 	name = 'blockduino_sketch.ino'
 	complete_name = os.path.join(save_path, name)
