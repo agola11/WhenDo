82be292df65e635f0da53181308897ee3d43f3c2
Ankush Gola <ankush@Ankushs-MacBook-Air.local>
2014-07-20 08:56:04 -0700

    new poll

--- blockduino/blockduino/urls.py
+++ blockduino/blockduino/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from core.views import HomeView, PollingAPIView, new_module, compiler
+from core.views import HomeView, PollingAPIView, new_module, poll_from_serial, compiler
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     url(r'^directives/when/', TemplateView.as_view(template_name='when.html'), name='when'),
     url(r'^directives/do/', TemplateView.as_view(template_name='do.html'), name='do'),
     url(r'^compile/', compiler, name='compiler'),
+    url(r'^poll_new/', poll_from_serial, name='poll'),
     url(r'^directives/display_table/', TemplateView.as_view(template_name='display_table.html'), name='table')
 )
